93a6cb6b546eb62ab9f43ee4d25e9f18e3e7ea63
Enzo Warner <enzo_warner@hotmail.com>
2024-09-12 21:54:33 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import CustomButton from './components/CustomButton/CustomButton';
+import TextInputBox from './components/TextInput/TextInput';
+import funcaoSoma from './actions/Funcoes';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.title}>Soma de Dois Números</Text>
+      <TextInputBox
+      value={number1}
+      onChangeText={setNumber1}
+      placeholder="Digite o primeiro número"
+      keyboardType="numeric"
+      />
+      <TextInputBox
+      value={number2}
+      onChangeText={setNumber2}
+      placeholder="Digite o segundo número"
+      keyboardType="numeric"
+      />
+      <CustomButton
+      title="Somar"
+      onPress={() => funcaoSoma(number1,number2)}
+      style={styles.button}
+      />
     </View>
   );
 }
@@ -17,4 +42,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  }
 });
+
